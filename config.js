f014a5b140b664af1f278aba3ad8e2357fe7d4f6
DITSHOOT <dimitri.ch42@gmail.com>
2024-01-04 22:37:16 +0100

    issue1

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
     prefix: "push!",
-    // token: "MTE5MjQxNzk5MjczNTIxMTU1MA.GU4clQ.DVOQa_0a2EDKIUQ7LsUre8NM6DBSzQgVhREWSw"
+    // token: ""
 }
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord  = require("discord.js")
 const bot = new Discord.Client({intents: 3276799})
 const config = require('./config')
 const express = require('express');
-const { EmbedBuilder } = require("@discordjs/builders");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
