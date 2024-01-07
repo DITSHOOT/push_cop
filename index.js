d4248760328017e1519819946d110909804d6b47
DITSHOOT <dimitri.ch42@gmail.com>
2024-01-07 18:58:35 +0100

    patch 2

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
-const Discord = require("discord.js");
+const Discord  = require("discord.js")
+const bot = new Discord.Client({intents: 3276799})
+const config = require('./config')
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const config = require('./config');
 
-const bot = new Discord.Client({ intents: 3276799 });
-let sendEmbed = true; // Variable pour activer/désactiver l'envoi de l'embed
-let reminderInterval = null; // Variable pour stocker l'identifiant de l'intervalle
+// Faire un facemath de citation (tournoi) || votez la meilleur citation, un 1vs1
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.TOKEN)
+
 
 app.listen(PORT, () => {
   console.log(`Le bot est en cours de lancement sur le port ${PORT}`);
 });
 
+
+
 bot.on('ready', () => {
+  
   const channel = bot.channels.cache.get('1163902591768477776');
   if (!channel) return console.error('Le salon spécifié est introuvable.');
-  
   channel.send('Le bot est maintenant connecté ! <@510818650307952640>');
-  console.log(`Connecté en tant que ${bot.user.tag}`);
+    console.log(`Connecté en tant que ${bot.user.tag}`);
 
-  const status = [
-    { name: 'je ne comprends pas ta question', type: Discord.ActivityType.Listening },
-    { name: 'désolé, mais tu as tort', type: Discord.ActivityType.Watching },
-    { name: "'n'hésitez pas à me solliciter !", type: Discord.ActivityType.Playing },
-  ];
+    
 
-  setInterval(() => {
+    let status = [
+      {
+        name: 'je ne comprends pas ta question',
+        type: Discord.ActivityType.Listening,
+      },
+      {
+        name: 'désolé, mais tu as tort',
+        type: Discord.ActivityType.Watching,
+      },
+      {
+        name: "'n'hésitez pas à me solliciters !",
+        type: Discord.ActivityType.Playing,
+      },
+    
+      
+    ]
+    
+  setInterval(() =>{
     let random = Math.floor(Math.random() * status.length);
     bot.user.setActivity(status[random]);
-  }, 3600000);
+  },3600000);
+  
 });
+
+
+let sendEmbed = true; // Variable pour activer/désactiver l'envoi de l'embed
+let lastHour; // Variable pour stocker la dernière heure à laquelle le rappel a été envoyé
 
 bot.on('messageCreate', (message) => {
   const channel = bot.channels.cache.get('1151478201823023214');
-  const now = new Date(new Date().toLocaleString("fr-FR", { timeZone: "Europe/Paris" }));
+
+  // Obtenez la date et l'heure actuelles en respectant le fuseau horaire français (UTC+1)
+  const now = new Date(new Date().toLocaleString("fr-FR", {timeZone: "Europe/Paris"}));
 
   if (message.content.startsWith(config.prefix + "pushgit") && message.guild && !message.author.bot) {
     message.delete();
+    // Commande manuelle pour envoyer l'embed
     sendReminder(channel, now);
   }
 
@@ -55,6 +78,7 @@ bot.on('messageCreate', (message) => {
   if (message.content.startsWith(config.prefix + "status")) {
     sendEmbed = !sendEmbed;
     lastHour = null;
+
     message.channel.send(`L'envoi de l'embed est maintenant ${sendEmbed ? 'activé' : 'désactivé'}.`);
     console.log(`Le statut de la commande est maintenant ${sendEmbed ? 'activé' : 'désactivé'}.`);
   }
@@ -75,10 +99,9 @@ function sendReminder(channel, now) {
     "Désolé, mais si tu n'utilises pas Git, tu as tort. Comme je l'ai dit un jour, utilise tes 2 mains, ça ira plus vite. Cela n'a aucun sens ? Je sais. Mais tu sais ce qui a aucun sens babouin pousse cassette ? Tu es atteint d'une logorrhée et à ton âge c'est grave. C'est une diarrhée verbale ! Pour y remédier, oublie pas de pousser toutes les 2 heures !",
   ]; // Remplacez cela par votre liste de messages
 
-
   const randomMessage = messagesaupiff[Math.floor(Math.random() * messagesaupiff.length)];
 
-  const embedMessage = new Discord.MessageEmbed()
+  const embedMessage = new Discord.EmbedBuilder()
     .setColor('#3498db')
     .setTitle(`<a:rappel:1185911636565954620> Rappel Quotidien à ${now.getHours()}h ! <a:rappel:1185911636565954620>`)
     .setDescription(randomMessage)
@@ -94,49 +117,139 @@ function sendReminder(channel, now) {
   channel.send({ embeds: [embedMessage] });
   channel.send('<@&1192744421201035374>');
 
+  lastHour = now.getHours();
   console.log(`Rappel quotidien envoyé à ` + now);
 }
 
-bot.on('messageCreate', (message) => {
-  if (message.content.startsWith(config.prefix + "status")) {
-    sendEmbed = !sendEmbed;
 
-    if (!sendEmbed && reminderInterval) {
-      clearInterval(reminderInterval);
-      reminderInterval = null;
-    }
+const roleId = '1192744421201035374'; // Remplacez par l'ID du rôle en questiiiooonnn
+const mentionnedrole = "<@&1192744421201035374>"; // idem mais c'est le ping
+const addEmoji = '✅'; // Emoji pour ajouter le rôle
+const removeEmoji = '❌'; // Emoji pour retirer le rôle
 
-    message.channel.send(`L'envoi de l'embed est maintenant ${sendEmbed ? 'activé' : 'désactivé'}.`);
-    console.log(`Le statut de la commande est maintenant ${sendEmbed ? 'activé' : 'désactivé'}.`);
+
+bot.on('messageCreate', async (message) => {
+  if (message.content.startsWith(config.prefix + "addrole") && message.guild && !message.author.bot) {
+    message.delete();
+    const mentionedRole = await message.guild.roles.fetch(roleId).catch(console.error);
+
+    const embedMessage = {
+      color: 0xF3AD53,
+      title: `Ajout de rôle ${mentionedRole.name}`,
+      description: `Clique sur ${addEmoji} pour ajouter le rôle\n Clique sur ${removeEmoji} pour retirer le rôle.\n\n**Rôle** : ${mentionedRole.name}\n\n(*tu peux le retirer ou l'ajouter à tout moment*)`,
+    };
+
+    const embedMessageObject = await message.channel.send({ embeds: [embedMessage] });
+    message.channel.send(`@everyone`)
+    await embedMessageObject.react(addEmoji);
+    await embedMessageObject.react(removeEmoji);
+
+    const filter = (reaction, user) => [addEmoji, removeEmoji].includes(reaction.emoji.name) && user.id === message.author.id;
+    const collector = embedMessageObject.createReactionCollector({ filter });
+
+    collector.on('collect', async (reaction) => {
+      // Obtient l'objet du membre qui a envoyé le message original
+      const member = message.guild.members.cache.get(message.author.id);
+    
+      // Filtre les utilisateurs qui ont réagi pour obtenir le premier utilisateur non-bot (et oui sinon ça sert à rien mdr)
+      const reactingUser = reaction.users.cache.filter(user => !user.bot).first();
+    
+      // Vérifie si la réaction est pour ajouter le rôle
+      if (reaction.emoji.name === addEmoji) {
+        // Ajoute le rôle au membre
+        member.roles.add(roleId);
+    
+        // Vérifie si un utilisateur non-bot a réagi
+        if (reactingUser) {
+          // Affiche un message dans la console indiquant le rôle ajouté et l'utilisateur
+          console.log(`Le rôle ${mentionedRole.name} a été ajouté à ${reactingUser.username}`);
+    
+          // Envoie un message dans le canal indiquant le succès
+          const replyMessage = await message.channel.send(`${reactingUser.toString()}, Le rôle **${mentionedRole.name}** vous a été ajouté avec succès.`);
+          
+          // Supprime le message après 5 secondes
+          setTimeout(() => {
+            replyMessage.delete().catch(console.error);
+          }, 5000);
+        } else {
+          // Si aucun utilisateur non-bot n'a réagi, affiche un message dans la console
+          console.log(`Le rôle ${mentionedRole.name} a été ajouté.`);
+        }
+      } else if (reaction.emoji.name === removeEmoji) {
+        // Retire le rôle du membre
+        member.roles.remove(roleId);
+    
+        // Vérifie si un utilisateur non-bot a réagi
+        if (reactingUser) {
+          // Affiche un message dans la console indiquant le rôle retiré et l'utilisateur
+          console.log(`Le rôle ${mentionedRole.name} a été retiré à ${reactingUser.username}`);
+    
+          // Envoie un message dans le canal indiquant le succès
+          const replyMessage = await message.channel.send(`${reactingUser.toString()}, Le rôle **${mentionedRole.name}** vous a été retiré avec succès.`);
+          
+          // Supprime le message après 5 secondes
+          setTimeout(() => {
+            replyMessage.delete().catch(console.error); // au cas ou si ça trop vite 
+          }, 5000);
+        } else {
+          // Si aucun utilisateur non-bot n'a réagi, affiche un message dans la console
+          console.log(`Le rôle ${mentionedRole.name} a été retiré.`);
+        }
+      }
+    
+      // Retire la réaction de l'auteur du message original
+      await reaction.users.remove(message.author.id);
+    });
+    
   }
 });
 
+
+
+
 bot.on('messageCreate', (message) => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(config.prefix)) return;
+  if (message.author.bot) return; // Ne répondez pas aux messages des bots
+  if (!message.content.startsWith(config.prefix)) return; // Vérifiez s'il commence par le préfixe
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === 'say' && message.member.permissions.has('ADMINISTRATOR')) {
-    const userMessage = args.join(' ');
+  if (command === 'say') {
+    // Vérifiez si l'utilisateur est un administrateur
+    if (!message.member.permissions.has('ADMINISTRATOR')) {
+      return message.reply('Seuls les administrateurs sont autorisés à utiliser cette commande.');
+    } 
 
+    // Récupère le message de l'utilisateur, en excluant le préfixe
+    const userMessage = args.join(' ');
     if (!userMessage) {
       return message.channel.send('Veuillez écrire un message.');
     }
 
+    // Supprime la commande de l'utilisateur
     message.delete();
+  
+    // Envoie le message personnalisé de l'utilisateur
     message.channel.send(userMessage);
   }
 });
 
+
 bot.on('messageCreate', (message) => {
   if (message.content === '!ping') {
-    const embed = new Discord.MessageEmbed()
+    const embed = new Discord.EmbedBuilder()
       .setTitle('Ping')
       .setDescription('Pong!')
       .setColor('#0099ff');
 
     message.channel.send({ embeds: [embed] });
   }
+
+    
 });
+
+
+
+// Au lieu d'utiliser setTimeout, j'utilise setInterval. setInterval permet d'exécuter une fonction à intervalles réguliers, tandis que setTimeout ne l'exécute qu'une seule fois (évite l'envoi de plein de message à la foi). Cela permet d'éviter de réinitialiser la temporisation à chaque message créé.
+
+// Stockage de l'identifiant de l'intervalle : J'ai ajouté une variable reminderInterval pour stocker l'identifiant de l'intervalle créé par setInterval. Cela nous permet de le manipuler plus tard, notamment pour l'arrêter lorsque l'envoi de l'embed est désactivé.
